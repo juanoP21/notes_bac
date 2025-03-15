a184e576f195247ef0929f1e67926723a6f8b721
Juan Manuel <juanmanuelperlazaperez@gmail.com>
2025-03-15 02:29:51 -0500

    initialize project with Express, Sequelize, and essential configurations

---
+++ index.js
@@ -0,0 +1,24 @@
+const { Sequelize, Model, DataTypes } = require("sequelize");
+
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+//Crear express app
+const app = express();
+
+
+app.use(express.static("public"));
+app.use(cors());
+const bodyParser = require("body-parser");
+
+// create application/json parser
+
+//Rutas
+app.use(bodyParser.json());
+
+app.use("/api/auth", require("./Routes/auth.js"));
+
+//Escuchar en puerto 4000
+app.listen(process.env.PORT, () => {
+  console.log("Servidor correindo en puerto", process.env.PORT);
+});
